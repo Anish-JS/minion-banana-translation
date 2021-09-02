321f6140a3e37f30c714b3ac1d35bfa1dc8af413
Anish-JS <anishjakka@gmail.com>
2021-09-02 21:06:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-var btn = document.querySelector("#btn")
-var txt = document.querySelector("#text-input")
-var outputDiv = document.querySelector("#display")
+var btn = document.querySelector("#btn");
+var txt = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#display");
 
+// console.log(outputDiv);
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL (text) {
-    return serverURL + "?"+ "text=" + text;
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
 
-function errorHandler(error){
-    console.log (" Error occured", error);
-    alert("Something wrong with server, Try again later");
-
+function errorHandler(error) {
+  console.log(" Error occured", error);
+  alert("Something wrong with server, Try again later");
 }
 
-function clickEventHandler(){
-    var input = txt.value;
-    
-    fetch(getTranslationURL(input))
-        .then (response => response.json)
-        .then (json => {
-            outputDiv.innerText = json.contents.translated;
-        })
-        .catch(errorHandler)
+function clickEventHandler() {
+  var input = txt.value;
+  //   console.log(typeof input);
+
+  fetch(getTranslationURL(input))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      console.log(translatedTxt);
+
+      outputDiv.innerText = translatedTxt;
+    })
+    .catch(errorHandler);
 }
 
-btn.addEventListener("click",clickEventHandler)
+btn.addEventListener("click", clickEventHandler);
